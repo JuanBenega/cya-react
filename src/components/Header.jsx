868bafd7461a24f73c80cc076edd65313474c2cb
juan benega <jmbenega@gmail.com>
2022-12-20 06:51:05 -0300

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from '../img/Logo.png'
 
 
 const Header = () => {
 
   return (
-    <h1>HEADER</h1>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='header'>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="Logo Cuerpo y Armonía" className="img-fluid" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className='navbarMenu'>
+          <Nav className="me-auto">
+            <Nav.Link href="#pricing" className='menuItem'>Nosotros</Nav.Link>
+            <NavDropdown className='menuServicios menuItem' title="Servicios" id="collasible-nav-dropdown">
+              <NavDropdown.Item className='menuItem' href="#action/3.1">Entrenamiento</NavDropdown.Item>
+              <NavDropdown.Item className='menuItem' href="#action/3.2">Masajes</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link className='menuItem' href="#deets">Artículos bienestar</Nav.Link>
+            <Nav.Link className='menuItem' href="#memes">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 
 }
