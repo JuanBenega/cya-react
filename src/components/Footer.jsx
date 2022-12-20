d483b3deca754852c457676c028de422c75f5679
juan benega <jmbenega@gmail.com>
2022-12-20 11:18:22 -0300

    add icons & fondo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,30 +4,34 @@ import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import LogoByN from '../img/LogoByN.png';
-import Whatsapp from '../img/whatsapp.png';
-import Facebook from '../img/facebook.png';
-import Instagram from '../img/instagram.png';
+import Whats from '../img/whatsapp.png';
+import Face from '../img/facebook.png';
+import Inst from '../img/instagram.png';
+import { Whatsapp, Facebook, Instagram } from "react-bootstrap-icons";
 
 const Footer = () => {
     return (
-        <Navbar>
-            <Row>
-                <Container>
+        <Navbar className='footer'>
+            <Row >
+                <Container fluid className='navFooter'> 
                     <Col lg='4' sm='11'>
                         <Nav className="me-auto">
                             <Nav.Link href="https://web.whatsapp.com">
-                                <img src={Whatsapp} alt="Whatsapp" width="30%" />
+                                {/* <img src={Whats} alt="Whatsapp" width="30%" /> */}
+                                <Whatsapp className='iconFooter'/>
                             </Nav.Link>
                             <Nav.Link href="#features">
-                                <img src={Facebook} alt="Whatsapp" width="30%" />
+                                {/* <img src={Face} alt="Whatsapp" width="30%" /> */}
+                                <Facebook className='iconFooter'/>
                             </Nav.Link>
                             <Nav.Link href="#pricing">
-                                <img src={Instagram} alt="Whatsapp" width="30%" />
+                                {/* <img src={Inst} alt="Whatsapp" width="30%" /> */}
+                                <Instagram className='iconFooter'/>
                             </Nav.Link>
                         </Nav>
                     </Col>
                     <Col lg='4' sm='11'>
-                        <Nav className="me-auto">
+                        <Nav className="me-auto menuFooter">
                             <Nav.Link href="#home">Bienestar</Nav.Link>
                             <Nav.Link href="#features">Entrenamiento</Nav.Link>
                             <Nav.Link href="#pricing">Masajes</Nav.Link>
