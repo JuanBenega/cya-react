ac6c5c40b2a2423751e16c8090278cb6095057f1
juan benega <jmbenega@gmail.com>
2022-12-21 12:24:48 -0300

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './styles/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
